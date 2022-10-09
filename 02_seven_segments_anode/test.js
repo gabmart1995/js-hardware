9a1e3ee76d14ad0b6a6eca75cd731209823afb04
gabmart1995 <gabmart1995@gmail.com>
2022-10-09 19:47:43 -0400

    wip: se utiliza el metodo reverse para inertir el array de puertos

--- 02_seven_segments_anode/test.js
+++ 02_seven_segments_anode/test.js
@@ -1,84 +1,105 @@
+function bitRead( buffer, posicion ) {
+  
+    let result = String.fromCharCode( buffer[ posicion ]);
+    result = Number.parseInt( result, 2 );
+
+    return result; 
+}
+  
+function transformarNumeroString( numero ) {
+    
+    let numeroResultado = numero.toString( 2 ); // transforma a binary string
+  
+    if ( numeroResultado.length < SEGMENTS_LENGTH ) {
+      
+      const espaciosFaltantes = SEGMENTS_LENGTH - numeroResultado.length;
+      // console.log({ numeroResultado,  indice, espaciosFaltantes });
+  
+      for ( let i = 0; i < espaciosFaltantes; i++ ) {
+        numeroResultado = "0" + numeroResultado;
+      }
+    }
+  
+    return numeroResultado;
+}
+  
+function transformarStringBinario( cadenaBinaria ) {
+  
+    const buffer = Buffer.alloc( SEGMENTS_LENGTH, 00 );
+  
+    for ( let i = 0 ; i < buffer.length; i++ ) {
+      buffer[i] = cadenaBinaria.charCodeAt( i );   
+    }
+  
+    return buffer
+}
+  
+function iluminarSegmento( buffer ) {
+    
+    for ( const index in PORTS ) {
+  
+      let bit = bitRead( buffer, index );
+      console.log({ puerto: PORTS[index], bit, position });
+    }
+  
+    // a
+    // board.digitalWrite( PORTS[0], 0 );
+  
+    // b
+    // board.digitalWrite( PORTS[1], 0 );
+  
+    // c
+    // board.digitalWrite( PORTS[2], 0 );
+  
+    // d
+    // board.digitalWrite( PORTS[3], 0 );
+  
+    // e
+    // board.digitalWrite( PORTS[4], 0 );
+  
+    // f
+    // board.digitalWrite( PORTS[5], 0 );
+  
+    // g
+    // board.digitalWrite( PORTS[6], 1 );
+  
+    if ( position >= 9 ) {
+      position = 0; // reset position
+      
+    } else {
+      position++;       
+  
+    }
+    
+    console.log('\n');
+}
+
+function main() {
+    iluminarSegmento( BINARY_SEGMENTS[position] );
+}
+  
 const SEGMENTS_LENGTH = 7;
-const BINARY_SEGMENTS = [
+const PORTS = [ 2, 3, 4, 5, 6, 7, 8 ].reverse();  // se invierte el orden de los puertos sin afectar el indice
+
+let BINARY_SEGMENTS = [
     0b1000000, // 0
-	  0b1111001, // 1
-  	0b0100100, // 2
-  	0b0110000, // 3
-  	0b0011001, // 4
-  	0b0010010, // 5
-  	0b0000010, // 6
-  	0b1111000, // 7
-  	0b0000000, // 8
-  	0b0011000, // 9
+    0b1111001, // 1
+    0b0100100, // 2
+    0b0110000, // 3
+    0b0011001, // 4
+    0b0010010, // 5
+    0b0000010, // 6
+    0b1111000, // 7
+    0b0000000, // 8
+    0b0011000, // 9
 ];
 
-/**
- * Lee el bit en la posicion especificada
- * @param {Buffer} buffer posicion del segmento
- * @param {number} position indice del segmento
- * @returns {number}
- */
- function bitRead( buffer, position ) {
-    return Number.parseInt( String.fromCharCode( buffer[position] ) );
-}
+BINARY_SEGMENTS = BINARY_SEGMENTS
+    .map( transformarNumeroString )
+    .map( transformarStringBinario );
 
-/**
- * Transforma la cadena de numeros binarios a string 
- * @param {number} position cadena de numeros
- * @returns {string}
- */
-function transformToString( position ) {
+// position of buffer array
+let position = 0;
 
-    // transforma el numero a string usando base binaria
-    let binaryString = position.toString( 2 ); 
-
-    if ( binaryString.length < SEGMENTS_LENGTH ) {
-
-        // calculamos el numeros de bit faltantes
-        let bytesEmpty = ( SEGMENTS_LENGTH - binaryString.length );
-
-        // agregamos los bit faltantes a la izquierda
-        for ( 
-            let position = 0; 
-            position < bytesEmpty; 
-            position++ 
-        ) {
-            binaryString = ( '0' + binaryString );
-        }
-    }
-
-    return binaryString;
-}
-
-/**
- * Transforma el resultado a bytes
- * @param {string} position digito en cadena de texto
- * @returns {Buffer}
- */
-function transformtoBuffer( position ) {
-    
-    // string a binario
-    const buffer = Buffer.alloc( SEGMENTS_LENGTH, 0, 'binary');
-        
-    for ( let i = 0; i < position.length; i++ ) {
-        buffer[i] = position.charCodeAt(i);
-    }
-
-    return buffer; 
-}
-
-// transformamos los bytes a string
-const arrayBuffer = BINARY_SEGMENTS
-    .map( transformToString )
-    .map( transformtoBuffer );
-
-// buffer binario
-console.log( arrayBuffer );
-
-// lectura de archivos binarios
-arrayBuffer.forEach(( buffer ) => {
-
-    for ( let i = ( buffer.length - 1 ); i >= 0; i-- ) {
-        console.log( bitRead( buffer, i ) );
-    }
-});
+// infinite timer
+setInterval( main, 1000 );
